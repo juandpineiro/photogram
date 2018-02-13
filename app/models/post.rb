a0e416c1e555a0a78ad701313a5e9e5cdbb6c1af
Juan Piñeiro <juandpineiro@gmail.com>
2018-02-12 22:27:41 -0400

    Add time ago to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   validates :image, presence: true
-
+  validates :caption, length: { minimum: 3, maximum: 300 }
   has_attached_file :image, styles: { :medium => '640x'}
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
