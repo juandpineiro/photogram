b8221b79ec45b059ff531364fb5d44f35f97d118
Juan Piñeiro <juandpineiro@gmail.com>
2018-02-15 10:12:39 -0400

    Fix placeholder image name for new posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,6 +13,6 @@ module ApplicationHelper
     return image_tag post.image.url(:medium), 
                       id: 'image-preview', 
                       class: 'img-responsive' if post.image.exists?
-    image_tag 'placeholder.jpg', id: 'image-preview', class: 'img-responsive'
+    image_tag 'placeholder.png', id: 'image-preview', class: 'img-responsive'
   end
 end
