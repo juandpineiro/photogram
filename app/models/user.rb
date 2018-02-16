43888bc8d74107532e3174ac5cd966b1ca15edc5
Juan Piñeiro <juandpineiro@gmail.com>
2018-02-15 22:35:10 -0400

    Add thumbnail profile pic in posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  has_attached_file :avatar, styles: { medium: '152x152#'}
+  has_attached_file :avatar, styles: { medium: '152x152#', thumb: '32x32#'}
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
